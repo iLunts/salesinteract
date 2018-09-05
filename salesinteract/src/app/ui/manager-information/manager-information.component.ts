811d590dd15484e69895a6ea749b5f602d8aa30b
Roman Luntsevich <shipik2008@gmail.com>
2018-09-05 21:59:39 +0300

    Create company edit page

--- salesinteract/src/app/ui/manager-information/manager-information.component.ts
+++ salesinteract/src/app/ui/manager-information/manager-information.component.ts
@@ -6,16 +6,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./manager-information.component.less']
 })
 export class ManagerInformationComponent implements OnInit {
+  isEdit: boolean = false;
+  
   managerList: any = [
-    { name: 'Coen van der Kolk', email: 'info@wald-int.com'},
-    { name: 'Grant Solomonesck', email: 'info@wald-int.com'},
-    { name: 'Grantowner Moriskovich', email: 'info@wald-int.com'},
+    { online: true, name: 'Coen van der Kolk', email: 'info@wald-int.com', image: 'http://dreamicus.com/data/face/face-06.jpg'},
+    { online: false, name: 'Grant Solomonesck', email: 'info@wald-int.com', image: 'https://images.asos-media.com/products/bejsbolka-chernyj-belyj-the-north-face-the-norm/10265685-1-black?$XL$?$XXL$&wid=300&fmt=jpeg&qlt=80,0&op_sharpen=0&resMode=sharp2&op_usm=1,0.4,6,1&iccEmbed=0&printRes=72'},
+    { online: true, name: 'Grantowner Moriskovich', email: 'info@wald-int.com', image: 'https://resources.stuff.co.nz/content/dam/images/1/n/t/2/j/z/image.related.StuffLandscapeSixteenByNine.620x349.1nqgze.png/1515968097908.jpg'},
   ];
 
   constructor() { }
 
   ngOnInit() {
-    
+  }
+
+  edit() {
+    this.isEdit = !this.isEdit;
   }
 
 }
