71ab51a027c26427819ab7df76168d344eac663a
Roman Luntsevich <shipik2008@gmail.com>
2018-11-08 17:28:32 +0300

    Work on fix sidebar create new company

--- salesinteract/src/app/components/new-company-tab/new-company-tab.component.ts
+++ salesinteract/src/app/components/new-company-tab/new-company-tab.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-new-company-tab',
   templateUrl: './new-company-tab.component.html',
-  styleUrls: ['./new-company-tab.component.less']
+  styleUrls: ['./new-company-tab.component.less'],
+  encapsulation: ViewEncapsulation.None
 })
 export class NewCompanyTabComponent implements OnInit {
+  isShowInternalNote: boolean = false;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  changeInternalNote(){
+    this.isShowInternalNote = !this.isShowInternalNote;
   }
 
 }
