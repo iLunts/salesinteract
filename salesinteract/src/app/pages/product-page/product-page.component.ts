7fef0d12ff7f08861cdea3938718cb3379c376ca
Roman Luntsevich <shipik2008@gmail.com>
2018-11-08 13:59:04 +0300

    Added quick view page

--- salesinteract/src/app/pages/product-page/product-page.component.ts
+++ salesinteract/src/app/pages/product-page/product-page.component.ts
@@ -12,6 +12,8 @@ export class ProductPageComponent implements OnInit {
     '#694efc', '#765efe', '#9484ff', '#245cd5', '#4079f3', '#5782e0', '#00cb73', '#00d266', '#00d48b', '#ff1a55', '#ff3967', '#ff5b7d', '#ff8e00', '#ff9242', '#ffa86b' 
   ];
 
+  colorDefault: string;
+
   constructor() { 
     this.productList = [
       {
@@ -33,6 +35,11 @@ export class ProductPageComponent implements OnInit {
   }
 
   ngOnInit() {
+    this.colorDefault = this.colorList[this.getRandomInt(this.colorList.length)];
+  }
+
+  getRandomInt(max) {
+    return Math.floor(Math.random() * Math.floor(max));
   }
 
   addNewProduct(){
