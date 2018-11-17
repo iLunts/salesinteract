d2299f226bbb767309083932eed82a688e176137
Roman Luntsevich <shipik2008@gmail.com>
2018-11-17 22:32:08 +0300

    Connect API

--- salesinteract/src/app/ui/task-history-list/task-history-list.component.ts
+++ salesinteract/src/app/ui/task-history-list/task-history-list.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit, Input } from '@angular/core';
   styleUrls: ['./task-history-list.component.less']
 })
 export class TaskHistoryListComponent implements OnInit {
+  showCount: number = 4;
 
   constructor() { }
 
