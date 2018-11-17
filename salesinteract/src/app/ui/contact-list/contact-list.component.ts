d1be1dee0659e1c30e96e856d2a9108907f124cf
Roman Luntsevich <shipik2008@gmail.com>
2018-11-18 00:32:31 +0300

    Create list of Contacten and Task statistics

--- salesinteract/src/app/ui/contact-list/contact-list.component.ts
+++ salesinteract/src/app/ui/contact-list/contact-list.component.ts
@@ -6,12 +6,53 @@ import { Component, OnInit, Input } from '@angular/core';
   styleUrls: ['./contact-list.component.less']
 })
 export class ContactListComponent implements OnInit {
+  contactDataList: any;
+  taskDataList: any;
+  contactTaskDataList: any;
 
   constructor() { }
 
   @Input() contactData: any;
-
+  @Input() taskData: any;
+  
   ngOnInit() {
+    this.contactDataList = Object.keys(this.contactData).map(key => ({ type: key, value: this.contactData[key] }));
+    this.taskDataList = Object.keys(this.taskData).map(key => ({ type: key, value: this.taskData[key] }));
+    
+    this.contactTaskDataList = this.contactDataList.concat(this.taskDataList);
+  }
+  
+  getColor(color){
+    let setColor = '';
+    
+    switch(color){
+      case 'lead': {
+        setColor = '#77dafd'
+        break;
+      }
+      case 'prospect': {
+        setColor = '#fff400'
+        break;
+      }
+      case 'customer': {
+        setColor = '#9ad100'
+        break;
+      }
+      case 'open': {
+        setColor = '#0096e0'
+        break;
+      }
+      case 'overdue': {
+        setColor = '#ff000a'
+        break;
+      }
+      default: {
+        setColor: '#fff'
+        break;
+      }
+    }
+
+    return setColor;
   }
 
 }
